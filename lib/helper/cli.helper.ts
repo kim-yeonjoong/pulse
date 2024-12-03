577a6b850bcc18f2911784cf81189d15f770851b
YeonJoong Kim <4367890+kim-yeonjoong@users.noreply.github.com>
2024-12-03 09:14:21 +0900

    fix: command 를 매 호출 시 재생성 하도록 수정, number 가 아닌 경우 기본 값 사용하도록 변경
    * fix: command 를 매 호출 시 재생성 하도록 수정
    
    * chore: boolean 타입으로 사용할 수 있도록 변경
    
    * fix: number 가 아닌 경우 기본 값 사용하도록 변경
    
    * chore: 불필요한 설정파일 삭제
    
    * chore: cli helper 테스트 코드 추가
    
    * chore: remove unused import

--- lib/helper/cli.helper.ts
+++ lib/helper/cli.helper.ts
@@ -1,11 +1,10 @@
 import { Argument, Command, Option } from 'commander';
 import path from 'node:path';
 
-const program = new Command();
-
 const DEFAULT_PATH = './';
 
 export const initCli = (): CliOptions => {
+  const program = new Command();
   program
     .name('pulse')
     .description('Keeping the pulse of your APIs under watch.')
@@ -18,7 +17,10 @@ export const initCli = (): CliOptions => {
       new Option('-m, --max <number>', 'max count of status logs')
         .default(100)
         .env('PULSE_STATUS_LOGS_MAX')
-        .argParser((value) => Number.parseInt(value, 10)),
+        .argParser((value) => {
+          const temporary = Number.parseInt(value, 10);
+          return Number.isNaN(temporary) ? 100 : temporary;
+        }),
     )
     .addOption(
       new Option('-o, --out <file-path>', 'output file path')
@@ -32,7 +34,10 @@ export const initCli = (): CliOptions => {
       )
         .default(5)
         .env('PULSE_FILE_CONCURRENCY')
-        .argParser((value) => Number.parseInt(value, 10)),
+        .argParser((value) => {
+          const temporary = Number.parseInt(value, 10);
+          return Number.isNaN(temporary) ? 5 : temporary;
+        }),
     )
     .addOption(
       new Option(
@@ -41,7 +46,10 @@ export const initCli = (): CliOptions => {
       )
         .default(50)
         .env('PULSE_EXECUTE_CONCURRENCY')
-        .argParser((value) => Number.parseInt(value, 10)),
+        .argParser((value) => {
+          const temporary = Number.parseInt(value, 10);
+          return Number.isNaN(temporary) ? 50 : temporary;
+        }),
     )
     .addOption(new Option('--json', 'Export current result to json file'))
     .parse(process.argv);
@@ -56,6 +64,6 @@ export const initCli = (): CliOptions => {
     OUTPUT_FILE_PATH: path.resolve(currentPath, String(options.out)),
     FILE_CONCURRENCY: Number(options.concurrency),
     EXECUTE_CONCURRENCY: Number(options.executeConcurrency),
-    EXPORT_JSON: options.json,
+    EXPORT_JSON: !!options.json,
   };
 };
--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig, coverageConfigDefaults } from 'vitest/config';
 export default defineConfig({
   test: {
     clearMocks: true,
+    restoreMocks: true,
     coverage: {
       provider: 'v8',
       exclude: [
         'commitlint.config.js',
-        'src/**/index.ts',
+        '**/index.ts',
         ...coverageConfigDefaults.exclude,
       ],
     },
